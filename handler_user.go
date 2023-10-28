d3a3213c85ba3aa2dad7cd3831bbaf970018a605
VishiK-on-github <vck281000@gmail.com>
2023-10-29 00:25:35 +0530

    apikey to fetch user done

--- handler_user.go
+++ handler_user.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"VishiK-on-github/rssagg/internal/auth"
 	"VishiK-on-github/rssagg/internal/database"
 	"encoding/json"
 	"fmt"
@@ -32,6 +33,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 		return
 	}
 
